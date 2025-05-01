65d889d17ca140f10e2676e1e6053e3d851ebc32
R1fky <rifkiyuda11@gmail.com>
2025-05-01 16:35:13 +0700

    testig import jawban dari pertanyaan tidak berhasil

---
+++ prisma/migrations/20250501091326_update_answer_text/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE `answer` MODIFY `answer_text` TEXT NOT NULL,
+    MODIFY `preProcess_text` TEXT NULL;
---
+++ prisma/migrations/20250501092459_update_varchar_fields/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE `answer` MODIFY `answer_text` VARCHAR(255) NOT NULL,
+    MODIFY `preProcess_text` VARCHAR(255) NULL;
